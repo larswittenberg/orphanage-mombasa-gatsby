1930efd2bfd06e69398c4ac517ad40b26beaf3dc
Lars Wittenberg <larswittenberg@googlemail.com>
2020-05-29 13:22:45 +0200

    UPDATE: Remove Google Maps API Key

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -40,7 +40,7 @@ const ContactPage = () => (
 				</div>
 				<div className="col-md-7">
 					<a href="https://www.google.com/maps/place/-4.085521,39.672258/" target="_blank" rel="noopener noreferrer">
-						<ResponsiveImage src="https://maps.googleapis.com/maps/api/staticmap?center=-4.075521,39.672258&zoom=14&scale=1&size=640x500&maptype=roadmap&key=AIzaSyDlwFh4f3pgBAq0eZ0gLHbHWWz99VU8SU0&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C-4.085521,39.672258" alt="Google Map of -4.075521,39.672258"/>
+						<ResponsiveImage src={`https://maps.googleapis.com/maps/api/staticmap?center=-4.075521,39.672258&zoom=14&scale=1&size=640x500&maptype=roadmap&key=${process.env.GOOGLE_MAPS_API_KEY}&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C-4.085521,39.672258`} alt="Google Map of -4.075521,39.672258"/>
 					</a>
 				</div>
 			</div>
